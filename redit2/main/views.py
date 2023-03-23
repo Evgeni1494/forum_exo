df933ae0ed7831716323415501b30adfd164d060
Evgeni1494 <yoni1@hotmail.fr>
2023-03-23 10:24:28 +0100

    Création du Dockerfile et Dokerisation.

--- redit2/main/views.py
+++ redit2/main/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+
+def hello_test(request):
+    return render(request,'hello_test.html')
+
 # Create your views here.
--- redit2/redit2/settings.py
+++ redit2/redit2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2rl2gvuf7#*y!0389h0jl$&j81(@j&d_pn0o1^-$mvchu4(7oq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
